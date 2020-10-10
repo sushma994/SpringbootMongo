63f4bb2a175b606965dfffa7294f5896386388e6
sushma994 <60339314+sushma994@users.noreply.github.com>
2020-10-10 16:50:01 +0530

    Update UserResource.java

--- src/main/java/com/mt/springmongo/UserResource.java
+++ src/main/java/com/mt/springmongo/UserResource.java
@@ -12,7 +12,7 @@ import java.util.Optional;
 /**
  * Rest controller which is responsible for retrieving {@link User}s.
  *
- * @author Mithun
+ * @author Sushma
  */
 @RestController
 @RequestMapping(value = "/api/users")
